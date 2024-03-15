92f2293bc4ee3492ed3a448b6ae07ff9627e7309
rodrigueseduardo <eduxdudu@icloud.com>
2024-03-15 15:35:16 -0300

    usando sistema de configuração ASPNET Core

--- Pokedex.api/Program.cs
+++ Pokedex.api/Program.cs
@@ -3,8 +3,8 @@ using Pokedex.api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connString = "DataSource = Pokedex.db";
-builder.Services.AddSqlite <PokedexContext> (connString);
+var connString = builder.Configuration.GetConnectionString("Pokedex");
+builder.Services.AddSqlite<PokedexContext>(connString);
 
 var app = builder.Build();
 
