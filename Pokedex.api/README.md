3187edcffd60f81b6a7e4c72b996f5fa01624cfa
rodrigueseduardo <eduxdudu@icloud.com>
2024-03-15 15:37:06 -0300

    update

--- Pokedex.api/README.md
+++
@@ -1,3 +0,0 @@
-Projeto criado para prática da linguagem C# com ASP.NET
-
-REST API com DTO e programação Assíncrona
--- Pokedex.api/obj/Debug/net8.0/Pokedex.api.AssemblyInfo.cs
+++ Pokedex.api/obj/Debug/net8.0/Pokedex.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1d2634b5b95e752845d641ccb4b7af2bb0e111")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f2293bc4ee3492ed3a448b6ae07ff9627e7309")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
