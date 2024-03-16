c55a61acf3a23b1fb22169c05f1f64d5d1c47d89
rodrigueseduardo <eduxdudu@icloud.com>
2024-03-16 11:13:19 -0300

    adicionado tipos dos pokemons

--- Pokedex.api/DataMigrations/20240316140612_SeedElements.Designer.cs
+++ Pokedex.api/DataMigrations/20240316140612_SeedElements.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pokedex.api.Data;
 
@@ -10,9 +11,11 @@ using Pokedex.api.Data;
 namespace Pokedex.api.DataMigrations
 {
     [DbContext(typeof(PokedexContext))]
-    partial class PokedexContextModelSnapshot : ModelSnapshot
+    [Migration("20240316140612_SeedElements")]
+    partial class SeedElements
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
--- Pokedex.api/obj/Debug/net8.0/Pokedex.api.AssemblyInfo.cs
+++ Pokedex.api/obj/Debug/net8.0/Pokedex.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3676d66ef6a28294e633831eac8189e8faab6f7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd9b158c363b569fa6552a72de3422cddec8e3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
